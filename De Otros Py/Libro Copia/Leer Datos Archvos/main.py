1b9503723c072f00ca515183e6e453fa4805674e
yrod15x <megasoldier15x@hotmail.com>
2024-02-02 12:45:29 -0500

    Delete De Otros Py/Libro Copia directory

--- De Otros Py/Libro Copia/Leer Datos Archvos/main.py
+++
@@ -1,8 +0,0 @@
-#Programa que usa una clase para leer un archivo y mostrar la suma de los kilos que reportan los productos
-
-from LeerDatos import *
-
-productos = LeerDatos()
-
-productos.leer_archivo("archivo.txt")
-productos.suma_valores()
